fe45af2d72a7f151c952cc5fbfa345a2abd5b941
Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>
2025-04-15 13:27:09 -0700

    fix: added fixes to Red Giant conda recipe including Maxon folder creation and PID env var fix
    Signed-off-by: Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>

--- conda_recipes/aftereffects-redgiant/recipe/build_win.sh
+++ conda_recipes/aftereffects-redgiant/recipe/build_win.sh
@@ -9,7 +9,9 @@ cp -r $SRC_DIR/redgiant $AE_PLUGINS_DIRECTORY
 # Version without update number
 RG_VERSION=${PKG_VERSION}
 RG_SERVICE_PATH="$AE_PLUGINS_DIRECTORY/redgiant/Red Giant Service.exe"
+MAXON_FOLDER_PATH="C:\ProgramData\Maxon\RLM"
 
+mkdir -p "$MAXON_FOLDER_PATH"
 
 # See https://docs.conda.io/projects/conda/en/latest/dev-guide/deep-dives/activation.html
 # for details on activation. The Deadline Cloud sample queue environments use bash
@@ -18,18 +20,21 @@ mkdir -p "$PREFIX/etc/conda/activate.d"
 mkdir -p "$PREFIX/etc/conda/deactivate.d"
 
 
+# TODO: Fix the timeout thing to use some kind of for loop to verify the PID is defined.
 cat <<EOF > "$PREFIX/etc/conda/activate.d/$PKG_NAME-$PKG_VERSION-vars.bat"
-set "RG_VERSION=$RG_VERSION"
-start "" "$RG_SERVICE_PATH" --noservice
+start "" cmd /k "echo Redshift License: %redshift_LICENSE% && "$RG_SERVICE_PATH" --noservice"
+timeout /t 5 >nul
 for /f "tokens=2 delims=," %%a in ('tasklist /fi "imagename eq Red Giant Service.exe" /nh /fo csv') do set "RG_PID=%%a"
+set "RG_VERSION=$RG_VERSION"
 EOF
 cat "$PREFIX/etc/conda/activate.d/$PKG_NAME-$PKG_VERSION-vars.bat"
 
 
 cat <<EOF > "$PREFIX/etc/conda/activate.d/$PKG_NAME-$PKG_VERSION-vars.sh"
-start "" "$RG_SERVICE_PATH" --noservice
+start "" cmd /k "echo Redshift License: %redshift_LICENSE% && "$RG_SERVICE_PATH" --noservice"
+sleep 5
+export "RG_PID=\$(ps -W | grep "Red Giant Service" | grep -v grep | awk '{print $4}')"
 export "RG_VERSION=$RG_VERSION"
-export "RG_PID=$(ps -W | grep "Red Giant Service" | grep -v grep | awk '{print $4}')"
 EOF
 cat "$PREFIX/etc/conda/activate.d/$PKG_NAME-$PKG_VERSION-vars.sh"
 
@@ -43,7 +48,8 @@ cat "$PREFIX/etc/conda/deactivate.d/$PKG_NAME-$PKG_VERSION-vars.bat"
 
 
 cat <<EOF > "$PREFIX/etc/conda/deactivate.d/$PKG_NAME-$PKG_VERSION-vars.sh"
-taskkill -f -t -pid \$RG_PID
+pkill -9 -P \$RG_PID
+kill -9 \$RG_PID
 unset RG_VERSION
 unset RG_PID
 EOF
