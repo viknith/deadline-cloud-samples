683d32048bfd9e94f4ab01ca75bbb8ab4feecb30
Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>
2025-09-10 17:48:52 -0700

    fix: switched from volume mount to copying files in to render AE, got a successful render
    Signed-off-by: Vikram Nithyanandam <143119182+viknith@users.noreply.github.com>

--- job_bundles/aftereffects_docker_render/submit.sh
+++ job_bundles/aftereffects_docker_render/submit.sh
@@ -4,11 +4,12 @@
 echo "Submitting After Effects Docker render job..."
 
 deadline bundle submit . \
-  -p ProjectFile="/Users/viknith/Downloads/titleFlip (converted).aep" \
+  -p ProjectFile="/Users/viknith/Downloads/Frame_CounterRedGiant(1).aep" \
   -p RenderQueueIndex=1 \
-  -p StartFrame=96 \
-  -p EndFrame=215 \
+  -p StartFrame=0 \
+  -p EndFrame=299 \
   -p OutputDir="./output" \
+  -p OutputFilename="FrameCounter_[#####].png" \
   -p ECR_REGISTRY="484745417699.dkr.ecr.us-west-2.amazonaws.com"
 
 echo "Job submitted"
